be9b925d8634cd820a2a727c55f8dba2805cc26c
Smswad <smswad2002@gmail.com>
2026-01-07 00:24:09 +0600

    header Facts is added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -29,6 +29,34 @@ const Home = () => {
 
             <img className='mt-10' src={hero} alt="hero.png" />
 
+            {/* Facts */}
+            <div className='flex flex-col text-center items-center w-full bg-[linear-gradient(125deg,#632EE3_0%,#9F62F2_100%)] text-white px-6 py-2 '>
+                <h1 className='font-bold text-[3rem] mt-20'>Trusted by Millions, Built for You</h1>
+                <div className='grid grid-cols-3 place-items-center mt-10 mb-10 mx-auto gap-15'>
+                    <section className=''>
+                        <h4 className='font-extralight text-[1rem]' >Total Downloads</h4>
+                        <h1 className='font-extrabold text-[4rem]'>29.6M</h1>
+                        <h4 className='font-extralight text-[1rem]'>21% more than last month</h4>
+                    </section>
+                    <section className=''>
+                        <h4 className='font-extralight text-[1rem]'>Total Reviews</h4>
+                        <h1 className='font-extrabold text-[4rem]'>906K</h1>
+                        <h4 className='font-extralight text-[1rem]'>46% more than last month</h4>
+                    </section>
+                    <section className=''>
+                        <h4 className='font-extralight text-[1rem]'>Active Apps</h4>
+                        <h1 className='font-extrabold text-[4rem]'>132+</h1>
+                        <h4 className='font-extralight text-[1rem]'>31 more will Launch</h4>
+                    </section>
+                </div>
+
+            </div>
+
+            {/* Apps */}
+            <div>
+                <h1>Trending Apps</h1>
+                <h3>Explore All Trending Apps on the Market developed by us</h3>
+            </div>
         </div>
     );
 };
