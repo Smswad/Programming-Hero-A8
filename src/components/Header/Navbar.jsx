80e9f44c8a9b0d04e133706e2490c945f26912f7
Smswad <smswad2002@gmail.com>
2026-01-19 23:28:17 +0600

    Apps page is added

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -52,7 +52,7 @@ const Navbar = () => {
                 </div>
                 <div className="navbar-end ">
                     {/* Contribute Button */}
-                    <Link to='https://github.com/Smswad/Programming-Hero-A8' className="btn w-36.25 h-11.25 mr-20 bg-[linear-gradient(125deg,#632EE3_0%,#9F62F2_100%)] text-white px-6 py-2 rounded-lg">
+                    <Link to='https://github.com/Smswad' className="btn w-36.25 h-11.25 mr-20 bg-[linear-gradient(125deg,#632EE3_0%,#9F62F2_100%)] text-white px-6 py-2 rounded-lg">
                         <span className='mr-px'><SiGithub /></span>
                         <span className='font-semibold text-[1rem]'>Contribute</span>
                     </Link>
--- src/pages/Allapps/Allapps.jsx
+++ src/pages/Allapps/Allapps.jsx
@@ -1,13 +1,40 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import Apps from '../Home/Apps';
 import App from '../App/App';
+import "../../App.css"
+import { useLoaderData } from 'react-router';
+import { Search } from 'lucide-react';
 
 const Allapps = () => {
+    const data = useLoaderData();
     return (
-        <div>
-           {/* <Apps></Apps> */}
-           <h1>Hi I am All Apps.</h1>
-           
+        <div className='bg-[#f5f5f5] flex flex-col items-center text-center inter-normal py-20'>
+            <div className='w-[80%]'>
+
+                <section>
+                    <h1 className='font-bold text-[3rem]'>Our All Applications</h1>
+                    <h3 className='mt-4 text-[#627382] text-[1.25rem]'>Explore All Apps on the Market developed by us. We code for Millions</h3>
+                </section>
+
+                <section className='flex justify-between items-center mt-10'>
+                    {/* App Count */}
+                    <h1 className='font-semibold text-[1.5rem]'>(20) Apps Found</h1>
+                    {/* Search Bar */}
+                    <label className="input">
+                        <Search />
+                        <input type="search" required placeholder="Search App" />
+                    </label>
+                </section>
+
+            </div>
+
+            <div className='grid grid-cols-4 gap-4'>
+                <Suspense fallback={<span>Loading.......</span>}>
+                    {
+                        data.map((singleApp) => <App key={singleApp.id} singleApp={singleApp}></App>)
+                    }
+                </Suspense>
+            </div>
         </div>
     );
 };
